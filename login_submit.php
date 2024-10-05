344f06d3648d75563b339df88798db7bc4f3d2ae
wesgitley <wesley.mutisya@strathmore.edu>
2024-10-05 13:26:18 +0300

    Start a session

--- login_submit.php
+++ login_submit.php
@@ -1,4 +1,10 @@
 <?php
+
+session_start();
+$_SESSION['user_id'] = $user['id'];
+echo "Login successful!";
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
